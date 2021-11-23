0d347cdc93c62652f1d1cfe7801541b6867ebccb
GehadAkram <63369821+GehadAkram@users.noreply.github.com>
2021-11-23 20:24:57 +0200

    add user functions

--- src/User.java
+++ src/User.java
@@ -4,8 +4,39 @@ enum UserStatus {ACTIVE, SUSPENDED, INRIDE, WAITING, OFFLINE}
 
 public class User extends Person{
 
-    int userId;
-    UserStatus stat;
+    private int userID;
+    private UserStatus status;
+    private static int count;
+    
+    public User() {
+        this.userID = count;
+        this.status = UserStatus.ACTIVE;
+        count++;
+    }
+
+    public User(int userID, String userName, String password, String email, String phoneNumber) {
+        super(userName, password, email, phoneNumber);
+        this.userID = count;
+        count++;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+    
+    public void register() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your information: ");
+        System.out.println("Username: ");
+        userName = input.nextLine();
+        System.out.println("Password: ");
+        password = input.nextLine();
+        System.out.println("Email (optional): ");
+        email = input.nextLine();
+        System.out.println("Phone Number: ");
+        phoneNumber = input.nextLine();
+        status = UserStatus.ACTIVE;
+    }
 
     userRequestRide rideRequest;
 
@@ -18,13 +49,17 @@ public class User extends Person{
         this.rideRequest = rideRequest;
     }
 
-    @Override
-    void login(Person person) {
-
-    }
-
-    @Override
-    void login() {
-        //stat = UserStatus.ACTIVE;
+    /*public void requestRide() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Where are you?");
+        String source = input.nextLine();
+        System.out.print("Where do you want to go?");
+        String destination = input.nextLine();
+        
+        Ride ride = new Ride(source, destination);
+    }*/
+    
+    public void update(){
+        
     }
 }
